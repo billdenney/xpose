7bf6976420b41856a5bc03292b2e99d1e98e93e8
guiastrennec <guiastrennec@gmail.com>
2019-10-26 18:43:42 +0200

    set vignettes and tests aside for now

---
+++ _vignettes/access_xpdb_data.R
@@ -0,0 +1,13 @@
+## ---- include = FALSE-----------------------------------------------------------------------------
+library(xpose)
+options(width = 100)
+
+xpdb <- xpdb_ex_pk
+
+knitr::opts_chunk$set(out.width = '75%',
+                      comment   = '',
+                      message   = FALSE)
+
+## ----demo print xpose_data------------------------------------------------------------------------
+xpdb # or print(xpdb)
+
